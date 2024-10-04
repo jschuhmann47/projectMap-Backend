2f24c941f6084f0eb330e6288c72ead2f1ab3aaf
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2024-10-04 08:37:12 -0300

    create functions

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -8,12 +8,14 @@ import { defaultStages, Permission, Stage, StageType } from './stage.schema'
 import { insensitiveRegExp } from './utils/escape_string'
 import { User } from 'src/user/user.schema'
 import { OrganizationChart } from './orgChart'
+import { OkrService } from 'src/herramientas/okr/okr.service'
 
 @Injectable()
 export class ProjectService {
     constructor(
         @InjectModel('Project') private projectModel: Model<Project>,
-        private userService: UserService
+        private userService: UserService,
+        private okrService: OkrService
     ) {}
 
     async getOne(id: string) {
@@ -146,6 +148,9 @@ export class ProjectService {
 
     async addChart(projectId: string, chart: OrganizationChart) {
         const project = await this.projectModel.findById(projectId)
+        if (project.chart) {
+            this.updateMissingAreas(project, chart)
+        }
         project.chart = chart
         project.save()
     }
@@ -223,5 +228,16 @@ export class ProjectService {
                 model: 'User',
                 select: '-password',
             })
+    }
+
+    private updateMissingAreas(project: Project, newChart: OrganizationChart) {
+        const existingAreas = project.chart.nodes
+        const newAreas = newChart.nodes
+        const deletedAreas = existingAreas
+            .filter((node) =>
+                newAreas.every((n) => n.data.label != node.data.label)
+            )
+            .map((n) => n.data.label)
+        this.okrService.updateMissingAreas(project._id.toString(), deletedAreas)
     }
 }
