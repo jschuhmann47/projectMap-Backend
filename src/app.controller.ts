b0119c2026263276e452f2c700c6a9f6a4a734b3
pabloH <pablohervida@est.frba.utn.edu.ar>
2024-11-11 15:54:05 -0300

    Primeros cambios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,21 @@
 import { Controller, Get } from '@nestjs/common'
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger'
 import { Tool } from './herramientas/tools'
 
+@ApiTags('Tools')
 @Controller()
 export class AppController {
     @Get('tools')
+    @ApiOperation({ summary: 'Retrieve available tools' })
+    @ApiResponse({
+        status: 200,
+        description: 'List of available tools',
+        schema: {
+            example: {
+                tool: ['Tool1', 'Tool2', 'Tool3'],
+            },
+        },
+    })
     getTools() {
         return {
             tool: Object.values(Tool),
